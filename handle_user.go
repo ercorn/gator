f0b7782c7b65f8a9ede8994fdcd7ca027a1bdb44
rcorn013 <rctexvn@gmail.com>
2025-09-02 09:42:36 -0400

    refactored handlers for register and login, and printUser into separate file

---
+++ handle_user.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ercorn/gator/internal/database"
+	"github.com/google/uuid"
+)
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.name)
+	}
+
+	ctx := context.Background()
+	user, err := s.db.CreateUser(ctx, database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      cmd.args[0],
+	})
+	if err != nil {
+		return fmt.Errorf("failed to create user: %w", err)
+	}
+
+	err = s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't set the user: %w", err)
+	}
+
+	fmt.Println("user was created:")
+	printUser(user)
+
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID: %v\n", user.ID)
+	fmt.Printf(" * Name: %v\n", user.Name)
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.name)
+	}
+
+	//check if current username exists in the database, error if not
+	ctx := context.Background()
+	_, err := s.db.GetUser(ctx, cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("failed, username doesn't exist in the database: %w", err)
+	}
+
+	err = s.cfg.SetUser(cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("couldn't set the user: %w", err)
+	}
+	fmt.Println("user has been set!")
+	return nil
+}
--- main.go
+++ main.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"os"
-	"time"
 
 	"github.com/ercorn/gator/internal/config"
 	"github.com/ercorn/gator/internal/database"
-	"github.com/google/uuid"
 	_ "github.com/lib/pq"
 )
 
@@ -43,58 +40,6 @@ func (c *commands) run(s *state, cmd command) error {
 func (c *commands) register(name string, f func(s *state, cmd command) error) error {
 	c.cmd_map[name] = f
 	return nil
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) != 1 {
-		return fmt.Errorf("usage: %s <name>", cmd.name)
-	}
-
-	//check if current username exists in the database, error if not
-	ctx := context.Background()
-	_, err := s.db.GetUser(ctx, cmd.args[0])
-	if err != nil {
-		return fmt.Errorf("failed, username doesn't exist in the database: %w", err)
-	}
-
-	err = s.cfg.SetUser(cmd.args[0])
-	if err != nil {
-		return fmt.Errorf("couldn't set the user: %w", err)
-	}
-	fmt.Println("user has been set!")
-	return nil
-}
-
-func handlerRegister(s *state, cmd command) error {
-	if len(cmd.args) != 1 {
-		return fmt.Errorf("usage: %s <name>", cmd.name)
-	}
-
-	ctx := context.Background()
-	user, err := s.db.CreateUser(ctx, database.CreateUserParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now().UTC(),
-		UpdatedAt: time.Now().UTC(),
-		Name:      cmd.args[0],
-	})
-	if err != nil {
-		return fmt.Errorf("failed to create user: %w", err)
-	}
-
-	err = s.cfg.SetUser(cmd.args[0])
-	if err != nil {
-		return fmt.Errorf("couldn't set the user: %w", err)
-	}
-
-	fmt.Println("user was created:")
-	printUser(user)
-
-	return nil
-}
-
-func printUser(user database.User) {
-	fmt.Printf(" * ID: %v\n", user.ID)
-	fmt.Printf(" * Name: %v\n", user.Name)
 }
 
 func main() {
