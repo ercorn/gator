9a04f16faee76c2d458e78f29e266077628ad355
rcorn013 <rctexvn@gmail.com>
2025-12-15 15:05:25 -0500

    added feed_follows join table

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -15,6 +15,16 @@ CREATE TABLE feeds (
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
 );
 
+CREATE TABLE feed_follows (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+    CONSTRAINT user_feed_unique UNIQUE(user_id, feed_id)
+);
+
 -- +goose Down
-DROP TABLE users;
+DROP TABLE feed_follows;
 DROP TABLE feeds;
+DROP TABLE users;
