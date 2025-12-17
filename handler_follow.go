7852c144a28b652f15a201d17735c7653b106eb6
rcorn013 <rctexvn@gmail.com>
2025-12-17 14:10:14 -0500

    added follow command

--- handler_follow.go
+++ handler_follow.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/ercorn/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 /*
@@ -12,9 +17,39 @@ look up feeds by URL.
 */
 func handlerFollow(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
-		return fmt.Errorf("usage: %s", cmd.name)
+		return fmt.Errorf("usage: %s <url>", cmd.name)
 	}
 
-	//
+	//get current user record
+	user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+	if err != nil {
+		return fmt.Errorf("failed to get user: %w", err)
+	}
+
+	//parse url and use it to get corresponding feed record
+	url := cmd.args[0]
+	feed, err := s.db.GetFeedByUrl(context.Background(), url)
+	if err != nil {
+		//couldn't get feed
+		return fmt.Errorf("failed to get feed: %w", err)
+	}
+
+	feed_follow, err := s.db.CreateFeedFollow(context.Background(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to follow the feed: %w", err)
+	}
+
+	fmt.Println("Feed followed successfully!")
+	fmt.Println("Username:", feed_follow[0].UserName)
+	fmt.Println("Feed name:", feed_follow[0].FeedName)
+	fmt.Println("==================================================")
+
 	return nil
 }
