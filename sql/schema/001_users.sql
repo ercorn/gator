911a3481327950743d276bf9856c1f88fbbb2cdf
rcorn013 <rctexvn@gmail.com>
2025-12-20 18:40:42 -0500

    reorganized migrations to fix issues and added new migration for last_fetched_at column

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -6,25 +6,5 @@ CREATE TABLE users (
     name TEXT UNIQUE  NOT NULL
 );
 
-CREATE TABLE feeds (
-    id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL,
-    url TEXT UNIQUE NOT NULL,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
-);
-
-CREATE TABLE feed_follows (
-    id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    feed_id UUID NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
-    CONSTRAINT user_feed_unique UNIQUE(user_id, feed_id)
-);
-
 -- +goose Down
-DROP TABLE feed_follows;
-DROP TABLE feeds;
 DROP TABLE users;
