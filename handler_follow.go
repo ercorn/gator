167adaf4b5475eb7ffdc48568b25f8a9e6ab2928
rcorn013 <rctexvn@gmail.com>
2025-12-19 22:24:10 -0500

    added unfollow command

--- handler_follow.go
+++ handler_follow.go
@@ -79,3 +79,26 @@ func handlerFollowing(s *state, cmd command, user database.User) error {
 	}
 	return nil
 }
+
+func handlerUnfollow(s *state, cmd command, user database.User) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("usage: %s <url>", cmd.name)
+	}
+
+	feed, err := s.db.GetFeedByUrl(context.Background(), cmd.args[0])
+	if err != nil {
+		return fmt.Errorf("failed to get feed from url: %w", err)
+	}
+
+	err = s.db.DeleteFeedFollow(context.Background(), database.DeleteFeedFollowParams{
+		UserID: user.ID,
+		FeedID: feed.ID,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to delete feed follow: %w", err)
+	}
+
+	fmt.Println("DELETED FOLLOWED FEED:", feed.Name, "FROM USER:", user.Name)
+
+	return nil
+}
--- main.go
+++ main.go
@@ -59,6 +59,7 @@ func main() {
 	cmds.register("feeds", handlerFeeds)
 	cmds.register("follow", middlewareLoggedIn(handlerFollow))
 	cmds.register("following", middlewareLoggedIn(handlerFollowing))
+	cmds.register("unfollow", middlewareLoggedIn(handlerUnfollow))
 
 	//parse arguments and run the requested command
 	if len(os.Args) < 2 {
